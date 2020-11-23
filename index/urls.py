b20ce20c1fab5ab6e2e181097232f3f8434be5cc
Michelle Meredith <meredith.m@husky.neu.edu>
2020-11-23 00:39:28 -0500

    Rating Form

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SearchView,  IndexView
+from .views import SearchView,  IndexView, RatingFormView
 
 app_name = 'index'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('search/', SearchView.as_view(), name='search'),
+    path('ratingform/', RatingFormView.as_view(), name='ratingform'),
 ]
